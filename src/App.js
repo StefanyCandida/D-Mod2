ae6255b4a0827cb6dd20672e144b7e720bec6e20
StefanyCandida <stefany.candida@gmail.com>
2022-04-17 10:04:04 -0300

    Alteração de arquivo input para header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
-import styled from 'styled-components'
-import Input from './Rotas/input'
+import Header from './Rotas/Header'
 
 const GlobalStyle = createGlobalStyle`
 *{
@@ -13,46 +12,15 @@ const GlobalStyle = createGlobalStyle`
     background-color:black;
   }
 
-  `
-
-const Header = styled.div`{
-display:flex; 
-justify-content:space-evenly;
-color:white;
-width:35%;
-}
-`
-const H1 = styled.h1`{
-  color:red;
-  }
-  `
- 
+  ` 
    
-
 export default class App extends React.Component {
   render() {
     return (
      
       <div> 
-         <GlobalStyle/>
-         <Header>
-            <H1>TODOFLIX</H1>          
-            <h2>Inicio</h2>
-            <details>
-               <summary>
-                 Categoria
-               </summary>
-                <ul>
-                  <li>Todos</li>            
-                  <li>Favoritos</li>
-                  <li>Já Vistos</li>
-                  <li>Adcionados</li>
-                </ul>                
-            </details>  
-              
-            </Header>   
-          <Input/>
-             
+         <GlobalStyle/>   
+            <Header/>          
           
               
        </div>
